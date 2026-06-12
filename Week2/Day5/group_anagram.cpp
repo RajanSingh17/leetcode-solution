6afe0d8de3ec0428e0388f04ad7dea013d11a419
RajanSingh17 <rajansingh6547@gmail.com>
2026-06-12 23:54:11 +0530

    Create group_anagram.cpp

---
+++ Week2/Day5/group_anagram.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for (auto& [key, group] : mp) {
+            result.push_back(group);
+        }
+
+        return result;
+    }
+};
